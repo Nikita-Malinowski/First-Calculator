b93df61b672b6a5f3b2ca9457d3325f8bb4506fb
Nikita-Malinowski <malinovskijnikita801@gmail.com>
2026-03-04 19:33:26 +0100

    calculator improved

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Taschenrechner Projekt gestartet")
+# Taschenrechner Projekt gestartet
+
+Zahl1 = float(input("Gib eine Zahl ein!: "))
+op = input('+, -, *, /, : ')
+Zahl2 = float(input("Gib nochmal eine Zahl ein!: "))
+
+if op == '+':
+    print(Zahl1 + Zahl2)
+elif op == '-':
+    print(Zahl1 - Zahl2)
+elif op == '*':
+    print(Zahl1 * Zahl2)
+elif op == '/':
+    print(Zahl1 / Zahl2)
+
+else:
+    print("Falsche Eingabe")
